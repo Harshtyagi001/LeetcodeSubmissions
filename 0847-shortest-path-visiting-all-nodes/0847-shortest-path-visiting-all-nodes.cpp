2966e4558bdae0a12f09146e138fe780ef279f6e
Harshtyagi001 <96621226+Harshtyagi001@users.noreply.github.com>
2023-09-18 01:37:53 +0530

    Time: 19 ms (74.97%), Space: 9.3 MB (83.28%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
-    
-int solve(unordered_map<int,vector<int>>&mp,int crr,unordered_map<int,unordered_map<int,int>>&chk,int hash,int n,vector<vector<int>>&dp){
-    // cout<<"crr: "<<crr<<endl;
-    // cout<<"hash and 1<<n:  "<<hash<<"   "<<(1<<n)-1<<endl;
-        if(hash==(1<<n)-1){return 1;}
-        if(dp[crr][hash]!=-1)return dp[crr][hash];
-        int mini=INT_MAX;
+    int shortestPathLength(vector<vector<int>>& graph) {
         
-        for(auto x:mp[crr]){
-            // cout<<"x:  "<<x<<endl;
-            if(chk[crr][x]==0){
-                // cout<<"enter"<<endl;
-                chk[crr][x]=1;
-                mini=min(mini,solve(mp,x,chk,hash|(1<<x),n,dp));
-                chk[crr][x]=0;
+        int n = graph.size();
+        
+        // track visited path and node
+        vector<vector<bool>>visited(n,vector<bool>(1<<n,false));
+        queue<pair<int,int>>Q;
+        // push all nodes and masks initially into queue and also mark visited
+        for(int i=0;i<n;i++)
+        {
+            Q.push({i,(1<<i)});
+            visited[i][(1<<i)] = true;
+        }
+        
+        int finalVal = (1<<n)-1;
+        int dist=-1;
+        // BFS travers in level order 
+        while(!Q.empty())
+        {
+            int s = Q.size();
+            dist++;
+            while(s--)
+            {
+               int node = Q.front().first;
+               int mask = Q.front().second;
+                Q.pop();
+                
+                // all nodes visited then return path length
+                if(mask == finalVal)return dist;
+                
+                // look at the mask of all neighbor nodes
+                for(int &adj: graph[node])
+                {
+                    // update mask as neighbor node visited
+                   int nextMask = mask | 1<<adj;
+                    
+                    // if neighbor node visited array contains updated mask then ignore to avoid cycle
+                    if(visited[adj][nextMask]==0){
+                    visited[adj][nextMask] = true;
+                    Q.push({adj,nextMask});
+                    }
+                }
             }
         }
-         // cout<<"mini:  "<<mini<<endl;
-        return dp[crr][hash]= mini==INT_MAX?mini:mini+1;
-    }
-    
-    int shortestPathLength(vector<vector<int>>& graph) {
-        unordered_map<int,vector<int>>mp;
-        int n=graph.size();
-        for(int i=0;i<n;i++){
-            for(auto j:graph[i])mp[i].push_back(j);
-        }
-        int ans=INT_MAX;
-        for(auto i:mp){
-            unordered_map<int,unordered_map<int,int>>chk;
-            vector<vector<int>>dp(graph.size()+1,vector<int>((1<<13),-1));
-            int val=solve(mp,i.first,chk,(1<<i.first),graph.size(),dp);
-            // cout<<"val: "<<i.first<<"  :  "<<val<<endl;
-            if(val!=INT_MAX)val--;
-            ans=min(ans,val);
-        }
         
-        return ans==INT_MAX?0:ans;
+        return dist;
     }
 };
