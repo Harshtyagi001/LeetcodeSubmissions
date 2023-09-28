8c93cc41e9c07a313b066e2f4a33e8fcea41e90d
Harshtyagi001 <96621226+Harshtyagi001@users.noreply.github.com>
2023-09-28 19:26:19 +0530

    Time: 22 ms (44.46%), Space: 8.5 MB (12.32%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>mpt,mps;
-        for(auto ch:t)mpt[ch]++;
-        int i=0,j=0;
-        string ans;
-        int mini=INT_MAX;
-        pair<int,int>minPr;
-        while(j<=s.size()){
-          if(j-i<t.size() or mps.size()<mpt.size()){mps[s[j]]++; j++;} // map ka size hi kam hai
-          else{
-              bool inc=false;  // check ki j increment krna hai ya nahi
-              int ct=0;
-              for(auto i:mpt){
-                  int actual=i.second;
-                  int get=0;
-                  if(mps.find(i.first)!=mps.end())get=mps[i.first];
-                  if(get<actual){
-                      inc=true;
-                      break;
-                  }
-                  else if(get>=actual)ct++;  // ki saari values cover hogyi
-              }
-              if(inc==true){if(j==s.size())break; mps[s[j++]]++;}  // saari values cover nahi hui toh j bdhao
-              else if(ct==mpt.size()){  // sari values aagyi tp min window find karo
-                  if(j-i<mini){
-                      mini=j-i;
-                      minPr={i,j};  // update if minimum
-                  }
-                 //   if(mps.size()>=mpt.size()){  // is condition li surity hai
-                           mps[s[i]]--;
-                           if(mps[s[i]]==0)mps.erase(mps[s[i]]);
-                           i++;
-                 //   }
-              }
-          }
+        if (t == "") return "";
+        unordered_map<char, int> mpt;
+        for (auto& x : t) mpt[x]++;
+        int len = s.size();
+        pair<int, int> pr = {-1, -1};
+        unordered_map<char, int> mps;
+        int have = 0, need = mpt.size(), i=0,j=0;
+        while (j<s.size()) {
+            mps[s[j]]++;
+            if (mpt.count(s[j]) && mps[s[j]] == mpt[s[j]]) {
+                have++;
+            }
+            while (have == need) {
+                if (j - i + 1 <= len) {
+                    pr.first = i, pr.second = j;
+                    len = j - i + 1;
+                }
+                mps[s[i]]--;
+                if (mpt.count(s[i]) && mps[s[i]] < mpt[s[i]]) {
+                    have--;
+                }
+                i++;
+            }
+              j++;
         }
-        return s.substr(minPr.first,minPr.second-minPr.first);   // to avoid memory leekage instead of making str each time I store th etsarting and endking index in pair
+        if (pr.second == -1 || pr.first == -1) return "";
+        
+        return s.substr(pr.first,pr.second-pr.first+1);
     }
 };
